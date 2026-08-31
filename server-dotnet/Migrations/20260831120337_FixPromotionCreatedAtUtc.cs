470cd19d6c2fc0f570fbb55dab3f18d2bae45d36
harishmdu26-ops <harishmdu26@gmail.com>
2026-08-31 17:51:12 +0530

    fix: add promotion CreatedAtUtc migration

---
+++ server-dotnet/Migrations/20260831120337_FixPromotionCreatedAtUtc.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AVASurface.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPromotionCreatedAtUtc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAtUtc",
+                table: "Promotions",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAtUtc",
+                table: "Promotions");
+        }
+    }
+}
